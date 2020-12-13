1cf3b6b8cec3d22df9ad54045869d8dca4d84254
marxygen@gmail.com <marxygen@gmail.com>
2020-12-13 20:01:31 +0300

    Wrapped in functions

--- bot/buttons.py
+++ bot/buttons.py
@@ -1,13 +1,17 @@
 from telebot import types
 from params import tables
+import random
 
-CATEGORY_CHOOSING_MARKUP = types.ReplyKeyboardMarkup()
-for x in tables.values():
-    CATEGORY_CHOOSING_MARKUP.row(types.KeyboardButton(x.capitalize()))
+def get_category_choosing_markup():
+    CATEGORY_CHOOSING_MARKUP = types.ReplyKeyboardMarkup()
+    for x in tables.values():
+        CATEGORY_CHOOSING_MARKUP.row(types.KeyboardButton(x.capitalize()))
+    return CATEGORY_CHOOSING_MARKUP
 
 def get_words_markup(words):
     words_markup = types.ReplyKeyboardMarkup()
-    for x in tables.values():
-        words_markup.row(types.KeyboardButton(x.capitalize()))
-
+    for x in range(len(words)):
+        word = random.choice(words)
+        words_markup.row(types.KeyboardButton(word.capitalize()))
+        words.remove(word)
     return words_markup
